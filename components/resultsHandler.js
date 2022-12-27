15acb17cbbe2b29579d623ee847cee2e29594d75
Jakub M <65038024+porezXD@users.noreply.github.com>
2022-12-27 19:19:21 +0100

    optimizing code

--- components/resultsHandler.js
+++ components/resultsHandler.js
@@ -1,16 +1,15 @@
 const checkForBMI = (bmi) => {
-    const changeColor = (value) => {
-        const firstEle = document.querySelector('.main__input')
-        const secondEle = document.querySelector('.main__results')
-        firstEle.style.borderColor = value
-        secondEle.style.borderColor = value
+    const resultHandler = (color, value) => {
+        const border = document.querySelectorAll('.border')
+        border.forEach(element => element.style.borderColor = color)
+        const text = document.querySelector('.bmiText')
+        text.innerHTML = value
     }
-    const p = document.querySelector('.bmiText')
-    if(bmi <= 20){ changeColor('grey'); p.innerHTML = 'Niedowaga' }
-    if(bmi > 20 && bmi <= 24.9){ changeColor('green'); p.innerHTML = 'Waga Prawidłowa' }
-    if(bmi > 25 && bmi <= 29.9){ changeColor('yellow'); p.innerHTML = 'Nadwaga' }
-    if(bmi > 30 && bmi <= 34.9){ changeColor('red'); p.innerHTML = 'Otyłość I stopnia' }
-    if(bmi > 35 && bmi <= 39.9){ changeColor('darkred'); p.innerHTML = 'Otyłość II stopnia' }
-    if(bmi >= 40){ changeColor('black'); p.innerHTML = 'Otyłość III stopnia' }
+    if(bmi <= 20){ resultHandler('grey', 'Niedowaga') }
+    if(bmi > 20 && bmi <= 24.9){ resultHandler('green', 'Waga Prawidłowa') }
+    if(bmi > 25 && bmi <= 29.9){ resultHandler('yellow', 'Nadwaga') }
+    if(bmi > 30 && bmi <= 34.9){ resultHandler('red', 'Otyłość I stopnia') }
+    if(bmi > 35 && bmi <= 39.9){ resultHandler('darkred', 'Otyłość II stopnia') }
+    if(bmi >= 40){ resultHandler('black', 'Otyłość III stopnia') }
 }
 export { checkForBMI }
