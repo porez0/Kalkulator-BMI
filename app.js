371a0bdfd114de84314861536842bad0f44bd9a2
Jakub M <65038024+porezXD@users.noreply.github.com>
2022-12-27 02:25:03 +0100

    result and color tweaks

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 // bmi = kg/m^2
-
-
-
 const createPopup = (mess) => {
     const p = document.createElement('p')
     p.classList.add('popup')
@@ -14,22 +11,42 @@ const createPopup = (mess) => {
     
 }
 
+
+
+const changeColor = (value) => {
+    const firstEle = document.querySelector('.main__input')
+    const secondEle = document.querySelector('.main__results')
+    firstEle.style.borderColor = value
+    secondEle.style.borderColor = value
+}
+const checkForBMI = (bmi) => {
+    const p = document.querySelector('.bmiText')
+
+    if(bmi <= 20){ changeColor('grey'); p.innerHTML = 'Niedowaga' }
+    if(bmi > 20 && bmi <= 24.9){ changeColor('green'); p.innerHTML = 'Waga Prawidłowa' }
+    if(bmi > 25 && bmi <= 29.9){ changeColor('yellow'); p.innerHTML = 'Nadwaga' }
+    if(bmi > 30 && bmi <= 34.9){ changeColor('red'); p.innerHTML = 'Otyłość I stopnia' }
+    if(bmi > 35 && bmi <= 39.9){ changeColor('darkred'); p.innerHTML = 'Otyłość II stopnia' }
+    if(bmi >= 40){ changeColor('black'); p.innerHTML = 'Otyłość III stopnia' }
+}
+
 const calcBMI = (weight, height) => {
-    const bmi = (weight / Math.pow(height * 0.01, 2)).toFixed(2) 
+    const bmi = (weight / Math.pow(height * 0.01, 2)).toFixed(1) 
+    checkForBMI(bmi)
     if(weight !== "" && height !== "") return bmi
     else {
         createPopup('Podano błędną wartość!')
         return 'Błąd'
     }
-
 }
+
 
 const btn = document.querySelector('.btn')
 btn.addEventListener('click', () => {
     const w = document.querySelector('.main__weight').value
     const h = document.querySelector('.main__height').value
     
-    document.querySelector('.span').innerHTML = calcBMI(w, h)    
+    document.querySelector('.bmiValue').innerHTML = calcBMI(w, h)    
 })
 
 addEventListener('mousemove', (e) => {
